d6d83f6230da77b283f90892692a7f9f1f0282ac
Kurtis <k.soroka@gmail.com>
2020-10-26 16:04:21 -0500

    continued

--- server/src/server.js
+++ server/src/server.js
@@ -10,93 +10,94 @@ const { generateString } = require('./utils');
 require('dotenv').config()
 
 app.use(morgan('combined'))
-.use(cors())
-.use(cookieParser());
+  .use(cors())
+  .use(cookieParser());
 var stateKey = 'spotify_auth_state';
 
 scopes = [
-    'playlist-read-private',
-    'playlist-read-collaborative',
-    'user-read-private',
-    'user-read-email',
-    'user-read-recently-played'
+  'playlist-read-private',
+  'playlist-read-collaborative',
+  'user-read-private',
+  'user-read-email',
+  'user-read-recently-played'
 ]
 
 app.get('/login', (_, res) => {
-    const state = generateString(16);
-    res.cookie(stateKey, state);
-  
-    return res.redirect(
-      `https://accounts.spotify.com/authorize?${queryString.stringify({
-        response_type: 'code',
-        client_id: process.env.CLIENT_ID,
-        scope: scopes.join(' '),
-        redirect_uri: process.env.REDIRECT_URI,
-        state: state,
-        show_dialog: true
+  const state = generateString(16);
+  res.cookie(stateKey, state);
+
+  return res.redirect(
+    `https://accounts.spotify.com/authorize?${queryString.stringify({
+      response_type: 'code',
+      client_id: process.env.CLIENT_ID,
+      scope: scopes.join(' '),
+      redirect_uri: process.env.REDIRECT_URI,
+      state: state,
+      show_dialog: true
+    })}`,
+  );
+});
+
+app.get('/loggedin', async (req, res) => {
+  // your application requests refresh and access tokens
+  // after checking the state parameter
+
+  const { code, state } = req.query;
+  const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  if (!state || state !== storedState) {
+    // return res.redirect(
+    //   `${FRONTEND_URI}/#${queryString.stringify({ error: 'state_mismatch' })}`
+    // );
+    return res.send("Mismatch")
+  }
+
+  res.clearCookie(stateKey);
+
+  try {
+    const params = {
+      client_id: env.process.CLIENT_ID,
+      client_secret: env.process.CLIENT_SECRET,
+      grant_type: 'authorization_code',
+      redirect_uri: env.process.REDIRECT_URI,
+      code
+    }
+
+    const { data: { access_token, refresh_token } } = await axiosRequest(params);
+
+    res.redirect(
+      `${FRONTEND_URI}/#${queryString.stringify({
+        access_token,
+        refresh_token
       })}`,
     );
-  });
-  
-  app.get('/loggedin', async (req, res) => {
-    // your application requests refresh and access tokens
-    // after checking the state parameter
-  
-    const { code, state } = req.query;
-    const storedState = req.cookies ? req.cookies[stateKey] : null;
-  
-    if (!state || state !== storedState) {
-      return res.redirect(
-        `${FRONTEND_URI}/#${queryString.stringify({ error: 'state_mismatch' })}`
-      );
+  } catch (e) {
+    res.redirect(`${FRONTEND_URI}/#${queryString.stringify({ error: 'invalid_token' })}`);
+  }
+});
+
+app.get('/refresh_token', async (req, res) => {
+  const { refresh_token } = req.query;
+
+  if (!refresh_token) return res.status(401).send('Invalid refresh token');
+
+  try {
+    const params = {
+      refresh_token,
+      grant_type: 'refresh_token',
+      client_id: CLIENT_ID,
+      client_secret: CLIENT_SECRET
     }
-  
-    res.clearCookie(stateKey);
-  
-    try {
-      const params = {
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET,
-        grant_type: 'authorization_code',
-        redirect_uri: CALLBACK_URL,
-        code
-      }
-  
-      const { data: { access_token, refresh_token } } = await axiosRequest(params);
-      
-      res.redirect(
-        `${FRONTEND_URI}/#${queryString.stringify({
-          access_token,
-          refresh_token
-        })}`,
-      );
-    } catch (e) {
-      res.redirect(`${FRONTEND_URI}/#${queryString.stringify({ error: 'invalid_token' })}`);
-    }
-  });
-  
-  app.get('/refresh_token', async (req, res) => {
-    const { refresh_token } = req.query;
-  
-    if (!refresh_token) return res.status(401).send('Invalid refresh token');
-  
-    try {
-      const params = {
-        refresh_token,
-        grant_type: 'refresh_token',
-        client_id: CLIENT_ID,
-        client_secret: CLIENT_SECRET
-      }
-  
-      const { data: { access_token } } = await axiosRequest(params);
-      
-      res.json({
-        access_token
-      });
-    } catch (e) {
-      res.status(401).send('Invalid token');
-    }
-  });
+
+    const { data: { access_token } } = await axiosRequest(params);
+
+    res.json({
+      access_token
+    });
+  } catch (e) {
+    res.status(401).send('Invalid token');
+  }
+});
 
 
 app.listen(process.env.PORT, () => {
