a51f455e6201a25201beba021c5d2e91cd97f45a
kwurty <kwurty@users.noreply.github.com>
2020-11-15 20:28:22 -0600

    implement a router

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -2,3 +2,10 @@ const express = require('express')
 const router = express.Router()
 const spotifyRoutes = require('./spotify/spotify');
 
+router.get('/login', (req,res) => {
+
+})
+
+router.get('/logged', (req, res) => {
+    
+})
--- server/src/routes/spotify/spotify.js
+++ server/src/routes/spotify/spotify.js
@@ -18,21 +18,7 @@ const Spotify = {
             url: 'https://api.spotify.com/v1/artists'
         })
     },
-    async getAccessTokenRefresh(refreshToken) {
-        const { data: { access_token } } = await Axios({
-            method: 'post',
-            url: 'https://accounts.spotify.com/api/token',
-            params: {
-                refresh_token: refreshToken,
-                grant_type: 'refresh_token',
-                client_id: process.env.CLIENT_ID,
-                client_secret: process.env.CLIENT_SECRET
-            },
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            },
-        });
-
-        return access_token
-    }
+    
 }
+
+module.exports = Spotify
