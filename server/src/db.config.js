64115f860e1ac4fd695afb3a8ec6f1b856256238
kwurty <k.soroka@gmail.com>
2022-02-01 21:17:09 -0600

    error check

--- server/src/db.config.js
+++ server/src/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config()
 
 console.log('attempting connection...');
-console.log(process.env);
+console.log(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`);
 mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`, {
     connectTimeoutMS: 1000,
     useNewUrlParser: true,
