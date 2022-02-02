f17e7ae132eaa0212441f1769c9f273e577250ba
kwurty <k.soroka@gmail.com>
2022-02-02 11:03:33 -0600

    better CORS response

--- server/src/server.js
+++ server/src/server.js
@@ -18,9 +18,10 @@ app.use(morgan('combined'))
 
 
 app.use(function (req, res, next) {
+  const regex = /^.+?[^\/:](?=[?\/]|$)/;
   let origin = Request.Headers["Origin"];
   let allowedDomains = ["http://localhost:8080", "https://artlists.kwurty.com", "https://artistplaylists.herokuapp.com"]
-  if (allowedDomains.includes(origin)) {
+  if (allowedDomains.includes(origin.match(regex)[0])) {
     res.header("Access-Control-Allow-Origin", origin);
   }
   res.header("Access-Control-Allow-Credentials", true);
