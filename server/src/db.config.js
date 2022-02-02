ea87a61a4673a1010da13ac4e6fc945a747955c8
kwurty <k.soroka@gmail.com>
2022-02-01 21:22:55 -0600

    authsource check

--- server/src/db.config.js
+++ server/src/db.config.js
@@ -5,6 +5,7 @@ console.log('attempting connection...');
 console.log(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`);
 mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE}`, {
     connectTimeoutMS: 1000,
+    authSource: "admin",
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
